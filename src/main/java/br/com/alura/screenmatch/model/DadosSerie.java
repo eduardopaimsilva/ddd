1c25bf7542954a4cea843fad7ed1c1d2a81629c4
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-14 12:53:46 -0300

    utilizando o while para manter a aplicação em execução até que o usuário decida sair.

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -6,10 +6,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                         @JsonAlias("totalSeasons") Integer totalTemporada,
-                         @JsonAlias ("imdbRating") String avaliacao,
-                         @JsonAlias("Genre") String genero,
-                         @JsonAlias("Actors") String atores,
-                         @JsonAlias("Poster") String poster,
-                         @JsonAlias("Plot")String sinopse){
-
+                         @JsonAlias ("imdbRating") String avaliacao) {
 }
