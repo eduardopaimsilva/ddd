4381a54155e141e8daed46f1de72ecee9b401fe9
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-20 23:19:30 -0300

    relacionando séries e episódios

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -23,7 +23,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie")
     private List<Episodio> episodios = new ArrayList<>();
 
 
