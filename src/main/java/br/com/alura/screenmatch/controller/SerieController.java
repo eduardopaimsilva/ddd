d413599334b42a4c4cde74e6ea5392464abd5eaf
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-27 15:54:03 -0300

    Criando um serviço para as séries

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,30 +1,23 @@
 package br.com.alura.screenmatch.controller;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
-import br.com.alura.screenmatch.model.Serie;
-import br.com.alura.screenmatch.repository.SerieRepository;
+import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
-    @Autowired
-    SerieRepository repositorio;
+@Autowired
+    private SerieService servico;
 
     @GetMapping("/series")
     public List<SerieDTO> ObterSerie(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
+
     }
-    @GetMapping("/inicio")
-    public String retornarInicio(){
-        return "Bem vindo";
-    }
+
 }
