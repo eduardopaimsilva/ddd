d6db85d45d1f8e5c7f8d9fcefa83cbaa57793bde
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-27 16:45:40 -0300

    Exibindo as top séries do banco

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,9 +1,11 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,10 +17,18 @@ public class SerieService {
     @Autowired
     SerieRepository repositorio;
 
-    public List<SerieDTO> obterTodasAsSeries(){
-            return repositorio.findAll()
-                    .stream()
+    public List<SerieDTO> obterTodasAsSeries() {
+        return converteDados(repositorio.findAll());
+
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+    private List<SerieDTO> converteDados(List<Serie>series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-            .collect(Collectors.toList());
+                .collect(Collectors.toList());
     }
 }
