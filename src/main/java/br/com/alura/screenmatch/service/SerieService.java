947459b6aa4116f109e7e6801c87e75eea310d71
eduardo paim silva <edups.ww.com@hotmail.com>
2024-05-27 17:22:41 -0300

    Mostrando as séries mais recentes

--- src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -15,7 +15,7 @@ import java.util.stream.Collectors;
 public class SerieService {
 
     @Autowired
-    SerieRepository repositorio;
+    private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries() {
         return converteDados(repositorio.findAll());
@@ -26,9 +26,13 @@ public class SerieService {
         return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
 
     }
-    private List<SerieDTO> converteDados(List<Serie>series){
+    private List<SerieDTO> converteDados(List<Serie> series){
         return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterLancamento() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
     }
 }
